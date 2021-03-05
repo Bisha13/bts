55d49f4e8b249543cc53e28ca0ca0dbf8e89061f
Bisha13 <ershoww@gmail.com>
2021-03-05 16:33:59 +0300

    First commit

---
+++ src/main/java/ru/bisha/bts/BtsApplication.java
@@ -0,0 +1,13 @@
+package ru.bisha.bts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bisha/bts/BtsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bisha.bts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
