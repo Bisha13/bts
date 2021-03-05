32475b84534d6632a4c70a6074eec11506f9646c
Bisha13 <ershoww@gmail.com>
2021-03-05 17:41:00 +0300

    index.html + IndexController created

---
+++ src/main/java/ru/bisha/bts/IndexController.java
@@ -0,0 +1,14 @@
+package ru.bisha.bts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping
+    public String showIndex() {
+        return "index";
+    }
+}
