1dd92e50ed6e808e0fa64c3901e3a5bb3d2ba835
Bisha13 <ershoww@gmail.com>
2021-03-06 14:37:00 +0300

    InsertRepos added

--- src/main/java/ru/bisha/bts/repo/UserInsertRepo.java
+++ src/main/java/ru/bisha/bts/repo/UserInsertRepo.java
@@ -13,10 +13,22 @@ public class UserInsertRepo {
     private EntityManager entityManager;
 
     @Transactional
-    public void insertWithQuery(String[] parsedLine) {
-        entityManager.createNativeQuery("INSERT INTO users (id, name) VALUES (?,?)")
+    public void insertWithQuery(final String[] parsedLine) {
+        entityManager.createNativeQuery("INSERT INTO users (" +
+                "id, " +
+                "theme, " +
+                "project_id, " +
+                "type, " +
+                "priority, " +
+                "executor_id, " +
+                "description) VALUES (?,?,?,?,?,?,?)")
                 .setParameter(1, parsedLine[1])
                 .setParameter(2, parsedLine[2])
+                .setParameter(3, parsedLine[3])
+                .setParameter(4, parsedLine[4])
+                .setParameter(5, parsedLine[5])
+                .setParameter(6, parsedLine[6])
+                .setParameter(6, parsedLine[7])
                 .executeUpdate();
     }
 }
