05b0aeb02c9d86cc8d1eb65f7a3673707734459e
Bisha13 <ershoww@gmail.com>
2021-03-06 14:27:15 +0300

    schema file updated

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,18 @@ CREATE TABLE users (
                               id INT AUTO_INCREMENT  PRIMARY KEY,
                               name VARCHAR(250) NOT NULL
 );
+
+CREATE TABLE projects (
+                       id INT AUTO_INCREMENT  PRIMARY KEY,
+                       name VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE projects (
+                          id INT AUTO_INCREMENT  PRIMARY KEY,
+                          theme VARCHAR(250) NOT NULL,
+                          project_id INT NOT NULL,
+                          type VARCHAR(250),
+                          priority VARCHAR(250),
+                          executor_id INT NOT NULL,
+                          description TEXT
+);
