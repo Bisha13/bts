b2bc61efbcf385ba7c2628e1368a9041c5b29772
Bisha13 <ershoww@gmail.com>
2021-03-07 11:25:11 +0300

    User Entity first persistence friendly setup

--- src/main/java/ru/bisha/bts/entity/UserEntity.java
+++ src/main/java/ru/bisha/bts/entity/UserEntity.java
@@ -1,16 +1,30 @@
 package ru.bisha.bts.entity;
 
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
+@Entity
+@Table(name = "users")
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "name")
     private String name;
 
+    @OneToMany(mappedBy = "executor", cascade = CascadeType.ALL)
     private List<TaskEntity> tasks;
 
 }
