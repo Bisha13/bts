23d15765b8b44049172c73663ff3551298b9e582
Bisha13 <ershoww@gmail.com>
2021-03-07 12:24:54 +0300

    UserSDJpaService added

---
+++ src/main/java/ru/bisha/bts/service/sdjpa/UserSDJpaService.java
@@ -0,0 +1,44 @@
+package ru.bisha.bts.service.sdjpa;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.bisha.bts.entity.UserEntity;
+import ru.bisha.bts.repo.UserRepo;
+import ru.bisha.bts.service.UserService;
+
+
+@Service
+public class UserSDJpaService implements UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public Set<UserEntity> findAll() {
+        Set<UserEntity> users = new HashSet<>();
+        userRepo.findAll().forEach(users::add);
+        return users;
+    }
+
+    @Override
+    public UserEntity findById(Integer id) {
+        return userRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public UserEntity save(UserEntity object) {
+        return userRepo.save(object);
+    }
+
+    @Override
+    public void delete(UserEntity object) {
+        userRepo.delete(object);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        userRepo.deleteById(id);
+    }
+}
