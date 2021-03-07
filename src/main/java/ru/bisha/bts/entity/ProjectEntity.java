7996a04a5cb89e46591f873da8d06c29e2499d7e
Bisha13 <ershoww@gmail.com>
2021-03-07 11:26:06 +0300

    Project Entity first persistence friendly setup

--- src/main/java/ru/bisha/bts/entity/ProjectEntity.java
+++ src/main/java/ru/bisha/bts/entity/ProjectEntity.java
@@ -3,12 +3,21 @@ package ru.bisha.bts.entity;
 import java.util.List;
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "projects")
 public class ProjectEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "name")
     private String name;
 
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
     private List<TaskEntity> tasks;
 }
