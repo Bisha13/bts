3da3d836d9b4c2511ccfb8bc3706c9aaa309dfe5
Bisha13 <ershoww@gmail.com>
2021-03-07 12:38:46 +0300

    TaskSDJpaService added

---
+++ src/main/java/ru/bisha/bts/service/sdjpa/TaskSDJpaService.java
@@ -0,0 +1,43 @@
+package ru.bisha.bts.service.sdjpa;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.bisha.bts.entity.TaskEntity;
+import ru.bisha.bts.repo.TaskRepo;
+import ru.bisha.bts.service.TaskService;
+
+@Service
+public class TaskSDJpaService implements TaskService {
+
+    @Autowired
+    private TaskRepo taskRepo;
+
+    @Override
+    public Set<TaskEntity> findAll() {
+        Set<TaskEntity> tasks = new HashSet<>();
+        taskRepo.findAll().forEach(tasks::add);
+        return tasks;
+    }
+
+    @Override
+    public TaskEntity findById(Integer id) {
+        return taskRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public TaskEntity save(TaskEntity object) {
+        return taskRepo.save(object);
+    }
+
+    @Override
+    public void delete(TaskEntity object) {
+        taskRepo.delete(object);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        taskRepo.deleteById(id);
+    }
+}
