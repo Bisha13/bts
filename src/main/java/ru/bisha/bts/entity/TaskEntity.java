432bd9c38f46d75ad44e7e2ae117d2cdbb7774ce
Bisha13 <ershoww@gmail.com>
2021-03-07 11:23:54 +0300

    Task Entity first persistence friendly setup

--- src/main/java/ru/bisha/bts/entity/TaskEntity.java
+++ src/main/java/ru/bisha/bts/entity/TaskEntity.java
@@ -1,29 +1,55 @@
 package ru.bisha.bts.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "tasks")
 public class TaskEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "theme")
     private String theme;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "project_id")
     private ProjectEntity project;
 
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
     private Type type;
 
+    @Column(name = "priority")
+    @Enumerated(EnumType.STRING)
     private Priority priority;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "executor_id")
     private UserEntity executor;
 
+    @Column(name = "description")
     private String description;
 
     public enum Type {
         BUG, TASK, ISSUE
     }
 
-    public enum Priority  {
+    public enum Priority {
         LOW, MEDIUM, HIGH
     }
 }
