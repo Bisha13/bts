686f43537798d9d40ae56f713e3c50a13ad3c50c
Bisha13 <ershoww@gmail.com>
2021-03-07 12:34:14 +0300

    ProjectSDJpaService added

---
+++ src/main/java/ru/bisha/bts/service/sdjpa/ProjectSDJpaService.java
@@ -0,0 +1,43 @@
+package ru.bisha.bts.service.sdjpa;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.bisha.bts.entity.ProjectEntity;
+import ru.bisha.bts.repo.ProjectRepo;
+import ru.bisha.bts.service.ProjectService;
+
+@Service
+public class ProjectSDJpaService implements ProjectService {
+
+    @Autowired
+    private ProjectRepo projectRepo;
+
+    @Override
+    public Set<ProjectEntity> findAll() {
+        Set<ProjectEntity> projects = new HashSet<>();
+        projectRepo.findAll().forEach(projects::add);
+        return projects;
+    }
+
+    @Override
+    public ProjectEntity findById(Integer id) {
+        return projectRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public ProjectEntity save(ProjectEntity object) {
+        return projectRepo.save(object);
+    }
+
+    @Override
+    public void delete(ProjectEntity object) {
+        projectRepo.delete(object);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        projectRepo.deleteById(id);
+    }
+}
