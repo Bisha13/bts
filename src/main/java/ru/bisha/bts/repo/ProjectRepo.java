8adfc1ffa317c2ef43cc993c820992558efc4704
Bisha13 <ershoww@gmail.com>
2021-03-07 22:17:07 +0300

    Repo refactored

--- src/main/java/ru/bisha/bts/repo/ProjectRepo.java
+++ src/main/java/ru/bisha/bts/repo/ProjectRepo.java
@@ -1,16 +1,7 @@
 package ru.bisha.bts.repo;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.bisha.bts.entity.ProjectEntity;
 
 public interface ProjectRepo extends CrudRepository<ProjectEntity, Integer> {
-
-    @Modifying
-    @Query(
-            value = "truncate table projects",
-            nativeQuery = true
-    )
-    void truncateMyTable();
 }
--- src/main/java/ru/bisha/bts/repo/TaskRepo.java
+++ src/main/java/ru/bisha/bts/repo/TaskRepo.java
@@ -1,16 +1,7 @@
 package ru.bisha.bts.repo;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.bisha.bts.entity.TaskEntity;
 
 public interface TaskRepo extends CrudRepository<TaskEntity, Integer> {
-
-    @Modifying
-    @Query(
-            value = "truncate table tasks",
-            nativeQuery = true
-    )
-    void truncateMyTable();
 }
