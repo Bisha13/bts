47c32adadb0be05d07bdf1d3f496b64e6879cdfa
Bisha13 <ershoww@gmail.com>
2021-03-07 22:30:18 +0300

    DB Cleaner Test created

---
+++ src/test/java/ru/bisha/bts/repo/DbCLeanerTest.java
@@ -0,0 +1,37 @@
+package ru.bisha.bts.repo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.bisha.bts.parser.FileParser;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+class DbCLeanerTest {
+
+    public static final String TEST_CSV = "src/test/resources/test.csv";
+
+    @Autowired
+    FileParser fileParser;
+
+    @Autowired
+    DbCLeaner dbCLeaner;
+
+
+    @BeforeEach
+    void setUp() {
+        fileParser.parseFileToDd(TEST_CSV);
+    }
+
+    @DisplayName("Test multiple cleaning of db")
+    @RepeatedTest(3)
+    void deleteAllData() {
+
+        assertDoesNotThrow(() -> {
+            dbCLeaner.deleteAllData();
+        });
+    }
+}
