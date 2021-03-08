6344ecdb7f50f371e5eb3d56a2717e4a430fe3ea
Bisha13 <ershoww@gmail.com>
2021-03-08 13:29:09 +0300

    ProjectSDJpaServiceTest findAll added

--- src/test/java/ru/bisha/bts/service/sdjpa/ProjectSDJpaServiceTest.java
+++ src/test/java/ru/bisha/bts/service/sdjpa/ProjectSDJpaServiceTest.java
@@ -4,14 +4,67 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.bisha.bts.model.Project;
+import ru.bisha.bts.model.User;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@DisplayName("Project persistence test")
 class ProjectSDJpaServiceTest extends PersistenceTest {
 
     @Autowired
     ProjectSDJpaService projectService;
+
+    @DisplayName("Test findAll + save")
+    @Test
+    void findAll() {
+
+        Set<Project> projectBefore = projectService.findAll();
+        int size = projectBefore.size();
+
+        Set<String> expected = new HashSet<>();
+        expected.add("1|bts");
+        expected.add("2|calculator");
+        expected.add("3|XO");
+
+        Set<String> actual = projectBefore
+                .stream()
+                .map(u -> u.getId() + "|" + u.getName())
+                .collect(Collectors.toSet());
+
+        assertEquals(expected, actual);
+
+        Project project1 = new Project();
+        project1.setName("Name1");
+        Project project2 = new Project();
+        project2.setName("Name2");
+        Project project3 = new Project();
+        project3.setName("Name3");
+        projectService.save(project1);
+        projectService.save(project2);
+        projectService.save(project3);
+
+        assertNotEquals(0, projectBefore.size());
+        Set<Project> projectsAfter = projectService.findAll();
+
+        expected.add("4|Name1");
+        expected.add("5|Name2");
+        expected.add("6|Name3");
+
+        Set<String> actual2 = projectsAfter
+                .stream()
+                .map(u -> u.getId() + "|" + u.getName())
+                .collect(Collectors.toSet());
+
+        assertEquals(expected, actual2);
+
+        int sizeAfter = projectsAfter.size();
+        assertEquals(size + 3,  sizeAfter);
+
+    }
 
     @DisplayName("Test save and delete")
     @Test
