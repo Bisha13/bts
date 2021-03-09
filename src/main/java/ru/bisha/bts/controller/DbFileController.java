342a2c407060ac170196a1e4415d261ff6b12721
Bisha13 <ershoww@gmail.com>
2021-03-09 23:32:07 +0300

    DbController added + choose-file.html

---
+++ src/main/java/ru/bisha/bts/controller/DbFileController.java
@@ -0,0 +1,41 @@
+package ru.bisha.bts.controller;
+
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.bisha.bts.model.dto.ResourceDto;
+import ru.bisha.bts.parser.FileParser;
+import ru.bisha.bts.parser.ResourceProvider;
+
+
+
+@Controller
+@RequestMapping("/")
+public class DbFileController {
+
+    @Autowired
+    private ResourceProvider resourceProvider;
+
+    @Autowired
+    private FileParser fileParser;
+
+    @RequestMapping
+    public String chooseFile(final Model model) {
+        Resource[] resources = resourceProvider.getResources();
+        model.addAttribute("resourcesAtr", resources);
+        model.addAttribute("resourceDto", new ResourceDto());
+        return "choose-file";
+    }
+
+    @PostMapping("/loadFile")
+    public String loadFile(@ModelAttribute(value = "resourceDto") final ResourceDto resourceDto)
+            throws IOException {
+        fileParser.parseFileToDd(resourceDto.getResource().getFile());
+        return "index";
+    }
+}
