0d3e79b80e3da18d0bd31410efdf369a0d27fbf9
Bisha13 <ershoww@gmail.com>
2021-03-10 00:15:13 +0300

    Simple index controller + html file changed

--- src/main/java/ru/bisha/bts/controller/IndexController.java
+++ src/main/java/ru/bisha/bts/controller/IndexController.java
@@ -1,23 +1,17 @@
 package ru.bisha.bts.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.bisha.bts.parser.FileParser;
 
 /**
  * Controller for index page.
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping("/index")
 public final class IndexController {
-
-    @Autowired
-    private FileParser parser;
 
     @RequestMapping
     public String showIndex() {
-        parser.parseFileToDd("src/main/resources/test.csv");
         return "index";
     }
 }
