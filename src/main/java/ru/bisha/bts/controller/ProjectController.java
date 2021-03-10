ac3e8aa472de9fdc96d82f0c3af41ea3502e973a
Bisha13 <ershoww@gmail.com>
2021-03-10 13:58:26 +0300

    Adding projects implemented

--- src/main/java/ru/bisha/bts/controller/ProjectController.java
+++ src/main/java/ru/bisha/bts/controller/ProjectController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.bisha.bts.model.entity.Project;
 import ru.bisha.bts.service.ProjectService;
@@ -21,5 +23,20 @@ public class ProjectController {
         Set<Project> projects = projectService.findAll();
         model.addAttribute("projectsAtr", projects);
         return "all-projects";
+    }
+
+
+    @GetMapping("/new")
+    public String addNewProject(final Model model) {
+        Project newProject = new Project();
+        model.addAttribute("projectAtr", newProject);
+        return "project-info";
+    }
+
+    @PostMapping
+    public String processNewProject(@ModelAttribute Project project) {
+        System.out.println(project.getName());
+        projectService.save(project);
+        return "redirect:/projects";
     }
 }
