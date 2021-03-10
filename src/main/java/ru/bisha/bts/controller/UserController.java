fae67a6647392078beae519877bc3b0397bef97e
Bisha13 <ershoww@gmail.com>
2021-03-10 14:09:00 +0300

    Adding users implemented

--- src/main/java/ru/bisha/bts/controller/UserController.java
+++ src/main/java/ru/bisha/bts/controller/UserController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.bisha.bts.model.entity.Project;
 import ru.bisha.bts.model.entity.User;
 import ru.bisha.bts.service.UserService;
 
@@ -21,5 +24,19 @@ public class UserController {
         Set<User> users = userService.findAll();
         model.addAttribute("usersAtr", users);
         return "all-users";
+    }
+
+    @GetMapping("/new")
+    public String addNewUser(final Model model) {
+        User newUser = new User();
+        model.addAttribute("userAtr", newUser);
+        return "user-info";
+    }
+
+    @PostMapping
+    public String processNewUser(@ModelAttribute User user) {
+        System.out.println(user.getName());
+        userService.save(user);
+        return "redirect:/users";
     }
 }
