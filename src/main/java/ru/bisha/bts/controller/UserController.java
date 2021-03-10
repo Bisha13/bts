cd002d726a2f457862f96b42eda5eb88e9812e58
Bisha13 <ershoww@gmail.com>
2021-03-10 15:32:41 +0300

    Updating of users added

--- src/main/java/ru/bisha/bts/controller/UserController.java
+++ src/main/java/ru/bisha/bts/controller/UserController.java
@@ -4,11 +4,8 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import ru.bisha.bts.model.entity.Project;
+import org.springframework.web.bind.annotation.*;
+import ru.bisha.bts.model.dto.UserDto;
 import ru.bisha.bts.model.entity.User;
 import ru.bisha.bts.service.UserService;
 
@@ -28,14 +25,28 @@ public class UserController {
 
     @GetMapping("/new")
     public String addNewUser(final Model model) {
-        User newUser = new User();
+        UserDto newUser = new UserDto();
         model.addAttribute("userAtr", newUser);
         return "user-info";
     }
 
+    @GetMapping("/update")
+    public String updateUser(@RequestParam(value = "userId") final int userId, final Model model) {
+        User user = userService.findById(userId);
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        model.addAttribute("userAtr", userDto);
+        return "user-info";
+    }
+
     @PostMapping
-    public String processNewUser(@ModelAttribute User user) {
-        System.out.println(user.getName());
+    public String processNewUser(@ModelAttribute UserDto userDto) {
+        User user = userService.findById(userDto.getId());
+        if (user == null) {
+            user = new User();
+        }
+        user.setName(userDto.getName());
         userService.save(user);
         return "redirect:/users";
     }
