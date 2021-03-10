ae96363b9ee2c58d5e19842246614ea0d80bcb61
Bisha13 <ershoww@gmail.com>
2021-03-10 12:49:59 +0300

    Task from projects added

--- src/main/java/ru/bisha/bts/controller/TaskController.java
+++ src/main/java/ru/bisha/bts/controller/TaskController.java
@@ -6,8 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.bisha.bts.model.entity.Project;
 import ru.bisha.bts.model.entity.Task;
 import ru.bisha.bts.model.entity.User;
+import ru.bisha.bts.service.ProjectService;
 import ru.bisha.bts.service.TaskService;
 import ru.bisha.bts.service.UserService;
 
@@ -19,23 +21,40 @@ import java.util.Set;
 public class TaskController {
 
     @Autowired
-    TaskService taskService;
+    private TaskService taskService;
 
     @Autowired
-    UserService userService;
+    private UserService userService;
+
+    @Autowired
+    private ProjectService projectService;
+
+    public static final String TASKS_ATR = "tasksAtr";
+
+    public static final String ALL_TASKS = "all-tasks";
 
     @RequestMapping
-    public String showTasks(Model model) {
+    public String showTasks(final Model model) {
         Set<Task> tasks = taskService.findAll();
-        model.addAttribute("tasksAtr", tasks);
-        return "all-tasks";
+        model.addAttribute(TASKS_ATR, tasks);
+        return ALL_TASKS;
     }
 
     @GetMapping("/from-user")
-    public String showTasks(@RequestParam(value = "userId", required = false) int id, Model model) {
+    public String showTasksFromUser(@RequestParam(value = "userId")
+                                        final int id, final Model model) {
         User user = userService.findById(id);
         List<Task> tasks = user.getTasks();
-        model.addAttribute("tasksAtr", tasks);
-        return "all-tasks";
+        model.addAttribute(TASKS_ATR, tasks);
+        return ALL_TASKS;
+    }
+
+    @GetMapping("/from-project")
+    public String showTasksFromProject(@RequestParam(value = "projectId")
+                                           final int id, final Model model) {
+        Project project = projectService.findById(id);
+        List<Task> tasks = project.getTasks();
+        model.addAttribute(TASKS_ATR, tasks);
+        return ALL_TASKS;
     }
 }
